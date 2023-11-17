4055d659fd1f5d06123d01d4ff9db998cdc97124
Aman Maharjan <mhrznamn068@gmail.com>
2023-11-17 17:08:28 +0545

    added flake config

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 
 @pytest.mark.django_db
 def test_user_create():
-  User.objects.create_user('aman', 'mhrznamn068@gmail.com', 'amanpassword')
-  assert User.objects.count() == 1
+    User.objects.create_user('aman', 'mhrznamn068@gmail.com', 'amanpassword')
+    assert User.objects.count() == 1
