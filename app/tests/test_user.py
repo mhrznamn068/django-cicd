ec55886b3f2f50b84ce5597d2d9475f4fae854e2
Aman Maharjan <mhrznamn068@gmail.com>
2023-11-17 16:51:05 +0545

    added pytest files

---
+++ app/tests/test_user.py
@@ -0,0 +1,9 @@
+import pytest
+
+from django.contrib.auth.models import User
+
+
+@pytest.mark.django_db
+def test_user_create():
+  User.objects.create_user('aman', 'mhrznamn068@gmail.com', 'amanpassword')
+  assert User.objects.count() == 1
